26fd5065bb79695b7eeb04851c4b61842d5872d2
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-08-17 20:29:21 -0700

    Update vehicle.lua

--- client/vehicle.lua
+++ client/vehicle.lua
@@ -70,3 +70,12 @@ CreateThread(function()
     AddTextComponentSubstringPlayerName("Dynasty 8 Real Estate")
     EndTextCommandSetBlipName(dynastyBlip)
 end)
+
+CreateThread(function() -- Trains
+    SwitchTrainTrack(0, true)
+    SwitchTrainTrack(3, true)
+    SetTrainTrackSpawnFrequency(0, 2 * 60 * 1000) -- 2 minutes in milliseconds
+    SetRandomTrains(1)
+      -- turn off/On doors opening
+      SetTrainsForceDoorsOpen(false)
+  end)
